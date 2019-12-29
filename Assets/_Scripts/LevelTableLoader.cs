a9090bb3f2df388419d4ed0fc04b1513b5fbabfc
Windson <im.windson@outlook.com>
2019-12-29 15:28:32 +0800

    Import excel reader plugins

---
+++ Assets/_Scripts/LevelTableLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class LevelTableLoader : MonoBehaviour
+{
+    private void Start()
+    {
+        TextAsset txtLevelTable = Resources.Load<TextAsset>("LevelTable.xml");
+        
+    }
+}
